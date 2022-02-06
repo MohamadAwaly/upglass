cca50c4002669c460ae3bbff70e076e82baa9571
Jiwaii <jeanyves.laurent@promsocatc.net>
2022-02-06 18:59:46 +0100

    Mise en place

---
+++ src/main/java/be/atc/managedbean/carAddBean.java
@@ -0,0 +1,20 @@
+package be.atc.managedbean;
+
+import be.atc.connection.EMF;
+import be.atc.entities.Car;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named
+@SessionScoped
+public class carAddBean implements Serializable {
+
+
+
+}
