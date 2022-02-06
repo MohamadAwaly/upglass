032a64f075e92c93cb452d53b34fd7c1774df609
Jiwaii <jeanyves.laurent@promsocatc.net>
2022-02-06 19:39:47 +0100

    Mise en place

--- src/main/java/be/atc/managedbean/carAddBean.java
+++ src/main/java/be/atc/managedbean/carAddBean.java
@@ -2,6 +2,7 @@ package be.atc.managedbean;
 
 import be.atc.connection.EMF;
 import be.atc.entities.Car;
+import be.atc.entities.Model;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -9,12 +10,30 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Named
 @SessionScoped
 public class carAddBean implements Serializable {
 
+    private Car car;
+    private Model model;
+
+
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+    public Car getCar() {
+        return car;
+    }
+    public void setModel(Model model) {
+        this.model = model;
+    }
+    public Model getModel() {
+        return model;
+    }
 
 
 }
